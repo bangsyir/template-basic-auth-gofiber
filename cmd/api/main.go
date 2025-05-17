0a6d1b2733787651b6a3ef750ce9245dddcc92a0
bangsyir <bangsyir@gmail.com>
2025-05-17 18:17:39 +0800

    add protected router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"go-auth/internal/domain/user"
 	"go-auth/internal/infrastruture/db"
 	repository "go-auth/internal/repository/user"
+	"go-auth/internal/transport/http/middleware"
 	http "go-auth/internal/transport/http/user"
 	"log"
 
@@ -25,6 +26,9 @@ func main() {
 	userService := user.NewService(userRepo, "this-is-secret-you-can't-see-don't-try-to-find")
 	userHandler := http.NewUserHandler(userService)
 
+	// add middleware to route
+	authMiddleware := middleware.JWTAuth("this-is-secret-you-can't-see-don't-try-to-find", userService)
+
 	// create fiber app
 	app := fiber.New(fiber.Config{
 		DisableStartupMessage: true,
@@ -34,6 +38,12 @@ func main() {
 	api := app.Group("/api/v1")
 	api.Post("/register", userHandler.Register)
 	api.Post("/login", userHandler.Login)
+
+	// protected route
+	protected := app.Group("/api/v1/protected")
+	protected.Use(authMiddleware)
+	protected.Get("/dashboard", userHandler.ProtectedRoute)
+	protected.Get("/me", userHandler.GetUser)
 
 	//Start server
 	log.Fatal(app.Listen(":3000"))
