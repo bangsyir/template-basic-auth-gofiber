31a2b839481b2139299328b19a558dfc083d0f12
bangsyir <bangsyir@gmail.com>
2025-05-17 18:15:23 +0800

    add FindUserById function

--- internal/domain/user/service.go
+++ internal/domain/user/service.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"time"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -62,6 +63,14 @@ func (s *Service) Login(ctx context.Context, username, password string) (*User, 
 	return user, accessToken, refreshToken, nil
 }
 
+func (s *Service) FindUserByID(ctx context.Context, userID uint) (*UserResponse, error) {
+	user, err := s.repo.FindUserById(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
 func (s *Service) checkIfAccessTokenIsExpired(expiresAt time.Time) bool {
 	// make current time minus 15 minues
 	threshhold := time.Now()
@@ -96,4 +105,49 @@ func generateSecureToken(length int) string {
 
 	token := base64.URLEncoding.EncodeToString(key)
 	return token
+}
+
+func (s *Service) RefreshToken(ctx context.Context, c *fiber.Ctx) (uint, error) {
+	// 1. get refresh token from cookie
+	refreshToken := c.Cookies("refresh_token")
+	if refreshToken == "" {
+		return 0, fiber.ErrUnauthorized
+	}
+	// 2. find token in database
+	storedToken, err := s.repo.FindRefreshToken(ctx, refreshToken)
+	if err != nil || storedToken.ExpiresAt.Before(time.Now()) {
+		return 0, fiber.ErrUnauthorized
+	}
+	// 3. Delete old refresh token
+	if err := s.repo.DeleteRefreshToken(ctx, refreshToken); err != nil {
+		return 0, err
+	}
+
+	// 4. generate new tokens
+	newAccessToken, err := s.generateAccessToken(storedToken.UserId)
+	if err != nil {
+		return 0, err
+	}
+	newRefreshToken, expiresAt, err := s.generateRefreshToken(ctx, storedToken.UserId)
+	if err != nil {
+		return 0, err
+	}
+
+	// 5. set new cookie
+	c.Cookie(&fiber.Cookie{
+		Name:     "access_token",
+		Value:    newAccessToken,
+		HTTPOnly: true,
+		Secure:   true,
+		MaxAge:   900, // 15 minutes
+	})
+	c.Cookie(&fiber.Cookie{
+		Name:     "refresh_token",
+		Value:    newRefreshToken,
+		HTTPOnly: true,
+		Secure:   true,
+		Expires:  expiresAt,
+	})
+
+	return storedToken.UserId, nil
 }
