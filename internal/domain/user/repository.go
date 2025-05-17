97f43074a2bdfb23d14183107c192d915f1d3071
bangsyir <bangsyir@gmail.com>
2025-05-17 18:14:59 +0800

    add FindUserById function

--- internal/domain/user/repository.go
+++ internal/domain/user/repository.go
@@ -5,7 +5,7 @@ import "context"
 type Repository interface {
 	Create(ctx context.Context, user *User) error
 	FindByUserName(ctx context.Context, username string) (*User, error)
-
+	FindUserById(ctx context.Context, userID uint) (*UserResponse, error)
 	// refresh token operation
 	CreateRefreshToken(ctx context.Context, token *RefreshToken) error
 	FindRefreshToken(ctx context.Context, token string) (*RefreshToken, error)
